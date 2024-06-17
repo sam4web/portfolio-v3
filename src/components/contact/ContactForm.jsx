5d730f2d5d234fd9bec509a626a541d20cf4138a
Sijal Manandhar <sijal.m06@gmail.com>
2024-06-17 13:27:16 +0545

    added handleSubmit on submit

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ name, email, message });
+  };
+
   return (
-    <form className=''>
+    <form onSubmit={handleSubmit}>
       <div className='bg-slate-200 dark:bg-slate-800 rounded-lg'>
         <div className='space-y-2 md:space-y-3.5 px-4 py-3 md:px-6 md:py-5 '>
           <h3 className='text-lg lg:text-xl font-montserrat font-semibold capitalize text-primary'>
@@ -12,8 +23,10 @@ const ContactForm = () => {
             <input
               type='text'
               placeholder='Enter your name'
-              name='name'
               id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
 
@@ -22,8 +35,10 @@ const ContactForm = () => {
             <input
               type='email'
               placeholder='Enter your email'
-              name='email'
               id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
 
@@ -31,8 +46,10 @@ const ContactForm = () => {
             <label htmlFor='message'>Message</label>
             <textarea
               placeholder='Type your Message...'
-              name='message'
               id='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
             />
           </div>
 
