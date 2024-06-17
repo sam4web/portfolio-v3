cb70ce7fe6a666205c2fff3728781c311f4e8d51
Sijal Manandhar <sijal.m06@gmail.com>
2024-06-17 20:01:32 +0545

    close model on scroll

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,12 +1,26 @@
 import useStore from '@/store';
+import { useEffect } from 'react';
 
 const Modal = () => {
   const senderName = useStore((state) => state.senderName);
   const closeModal = useStore((state) => state.closeModal);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      closeModal();
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [closeModal]);
+
   return (
-    <div className='bg-opacity-40 block w-full h-screen fixed top-0 left-0 bg-slate-950 z-20'>
-      <div className='size-full flex-center'>
+    <div
+      className='bg-opacity-40 block w-full h-screen fixed top-0 left-0 bg-slate-950 z-20'
+      onClick={closeModal}
+    >
+      <div className='size-full flex-center px-3.5'>
         <div className='bg-light-body dark:bg-dark max-w-lg w-full p-6 rounded-lg'>
           <div className='space-y-3 mb-8'>
             <h3 className='text-lg lg:text-xl font-montserrat font-semibold capitalize text-primary'>
