0fb88c954f1197f877225a55b8e04b4bf5faa67a
Sijal Manandhar <sijal.m06@gmail.com>
2024-12-11 19:39:41 +0545

    updated `resume.pdf` file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import usePageTitle from '@/hooks/usePageTitle';
-import { Document, Page, pdfjs } from 'react-pdf';
-import { FiDownload, FiZoomIn, FiZoomOut } from 'react-icons/fi';
-import resume from '@/resources/resume.pdf';
+import { useState } from "react";
+import usePageTitle from "@/hooks/usePageTitle";
+import { Document, Page, pdfjs } from "react-pdf";
+import { FiDownload, FiZoomIn, FiZoomOut } from "react-icons/fi";
+import resume from "@/resources/resume2024.pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
+  "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url
 ).toString();
 
 const Resume = () => {
-  usePageTitle('Resume | Sijal Manandhar');
+  usePageTitle("Resume | Sijal Manandhar");
 
   const [showDownloadBtn, setShowDownloadBtn] = useState(false);
   const [scale, setScale] = useState(1.2);
@@ -20,11 +20,11 @@ const Resume = () => {
   };
 
   return (
-    <section className='mt-10 section-spacing pb-6'>
+    <section className="mt-10 section-spacing pb-6">
       {showDownloadBtn && (
-        <div className='flex px-4 sm:px-7 lg:max-w-3xl mx-auto gap-2'>
+        <div className="flex px-4 sm:px-7 lg:max-w-3xl mx-auto gap-2">
           <button
-            className='btn p-2 text-2xl'
+            className="btn p-2 text-2xl"
             onClick={() => setScale((prev) => (prev += 0.1))}
             disabled={scale >= 1.7}
           >
@@ -32,30 +32,30 @@ const Resume = () => {
           </button>
 
           <button
-            className='btn p-2 text-2xl'
+            className="btn p-2 text-2xl"
             onClick={() => setScale((prev) => (prev -= 0.1))}
             disabled={scale <= 0.7}
           >
             <FiZoomOut />
           </button>
 
-          <a className='btn p-2 text-2xl' href={resume} download='resume.pdf'>
+          <a className="btn p-2 text-2xl" href={resume} download="resume.pdf">
             <FiDownload />
           </a>
         </div>
       )}
-      <div className='flex-center'>
-        <div className='overflow-scroll rounded-md'>
+      <div className="flex-center">
+        <div className="overflow-scroll rounded-md">
           <Document
             file={resume}
             onLoadSuccess={onDocumentLoadSuccess}
-            className='text-responsive'
+            className="text-responsive"
           >
             <Page
               pageNumber={1}
               renderTextLayer={false}
               renderAnnotationLayer={false}
-              canvasBackground='transparent'
+              canvasBackground="transparent"
               scale={scale}
             />
           </Document>
